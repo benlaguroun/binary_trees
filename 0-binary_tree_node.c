b957c9d321bb99b1489fdba8739da2db1d7481eb
benlaguroun <benlaguroun@gmail.com>
2023-10-31 10:49:59 -0700

    0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,26 +1,24 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * main - Entry point for testing binary tree creation and printing.
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
  *
- * Return: Always 0 (Success)
+ * Return: Pointer to the newly created node
+ *         NULL on failure
  */
-
-int main(void)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *root;
+	binary_tree_t *new;
 
-	root = binary_tree_node(NULL, 98);
-
-	root->left = binary_tree_node(root, 12);
-	root->left->left = binary_tree_node(root->left, 6);
-	root->left->right = binary_tree_node(root->left, 16);
-
-	root->right = binary_tree_node(root, 402);
-	root->right->left = binary_tree_node(root->right, 256);
-	root->right->right = binary_tree_node(root->right, 512);
-
-	binary_tree_print(root);
-	return (0);
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
 }
+
