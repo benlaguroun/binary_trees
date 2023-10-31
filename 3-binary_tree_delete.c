c71f820f12e1441c3047b12898a311cac58b3481
benlaguroun <benlaguroun@gmail.com>
2023-10-31 11:49:50 -0700

    3- fille

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree.
+ *
+ * @tree: Pointer to the binary tree to delete.
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
+}
+
