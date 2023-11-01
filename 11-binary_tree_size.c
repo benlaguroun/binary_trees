0a9831371eb47890f3289dc9fd636e6a259e19e4
benlaguroun <benlaguroun@gmail.com>
2023-11-01 02:58:19 -0700

    11- fille

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures the size of a binary tree.
+ *
+ * @tree: Pointer to the tree to measure the size of.
+ *
+ * Return: The size of the tree, or 0 if the tree is NULL.
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
